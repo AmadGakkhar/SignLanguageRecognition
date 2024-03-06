ad59de9b7fd1dd40fa16d7fe4f2ab3c7750a68aa
AmadUdDinGakkhar <a_gakkhar@hotmail.com>
2024-03-06 07:23:07 +0500

    added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="SignLanguageREcognition",
+    version="0.0.0",
+    author="AmadGakkhar",
+    author_email="amdgakkhar@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
